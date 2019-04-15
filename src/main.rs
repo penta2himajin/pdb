c43a8ae6b892cc5979662b685dc59d6cc17f5da7
Kenya Nara <penta2himajin@gmail.com>
2019-04-15 22:38:34 +0900

    add proc_trace func and read func

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+extern crate nix;
+
+use std::io;
+use std::ptr::null_mut;
+use nix::unistd::Pid;
+use nix::sys::ptrace::{ptrace, Request};
+
 fn main() {
-    println!("Hello, world!");
+    println!("pdb written by penta2himajin.");
+    let pid = Pid::from_raw(read::<i32>());
+    proc_trace(pid);
+}
+
+fn read<T: std::str::FromStr>() -> T {
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Couldn't receive correct input");
+    return input.trim().parse().ok().unwrap();
+}
+
+fn proc_trace(pid: Pid){
+    unsafe{
+        ptrace(Request::PTRACE_ATTACH, pid, null_mut(), null_mut());
+    }
 }
